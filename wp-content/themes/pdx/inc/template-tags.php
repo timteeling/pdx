1b65de33cc3cc50501fb4a12a5b790d55364b813
Tim Teeling <tteeling@tenable.com>
2015-05-23 17:01:10 -0400

    added post_thumbnail support

--- wp-content/themes/pdx/inc/template-tags.php
+++ wp-content/themes/pdx/inc/template-tags.php
@@ -37,6 +37,39 @@ function the_posts_navigation() {
 }
 endif;
 
+if ( ! function_exists( 'pdx_post_thumbnail' ) ) :
+/**
+ * Display an optional post thumbnail.
+ *
+ * Wraps the post thumbnail in an anchor element on index views, or a div
+ * element when on single views.
+ *
+ * @since Twenty Fifteen 1.0
+ */
+function pdx_post_thumbnail() {
+	if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
+		return;
+	}
+
+	if ( is_singular() ) :
+	?>
+
+	<div class="post-thumbnail">
+		<?php the_post_thumbnail(); ?>
+	</div><!-- .post-thumbnail -->
+
+	<?php else : ?>
+
+	<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true">
+		<?php
+			the_post_thumbnail( 'post-thumbnail', array( 'alt' => get_the_title() ) );
+		?>
+	</a>
+
+	<?php endif; // End is_singular()
+}
+endif;
+
 if ( ! function_exists( 'the_post_navigation' ) ) :
 /**
  * Display navigation to next/previous post when applicable.
--- wp-content/themes/pdx/template-parts/content-single.php
+++ wp-content/themes/pdx/template-parts/content-single.php
@@ -4,6 +4,11 @@
  */
 ?>
 
+<?php
+	// Post thumbnail.
+	pdx_post_thumbnail();
+?>
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
