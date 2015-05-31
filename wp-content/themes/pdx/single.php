f200a3331b02bb65ea23abbbf77501debf1d8392
Tim Teeling <tteeling@tenable.com>
2015-05-31 11:08:52 -0400

    added comments

--- wp-content/themes/pdx/single.php
+++ wp-content/themes/pdx/single.php
@@ -14,6 +14,18 @@ get_header(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
 
+			
+			<div class="comment-section container">
+			<h2>Comments</h2>
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
+			</div>
+
+
 		<?php endwhile; // end of the loop. ?>
 
 		</main><!-- #main -->
